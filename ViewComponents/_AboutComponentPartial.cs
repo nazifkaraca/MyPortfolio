d8045b3c7d05b19489cf5774d9815f4fd59b084f
nazifkaraca <nazif808@gmail.com>
2024-11-13 19:35:08 +0300

    MyPortfolio: Front-end configurations completed. Back-end connections will be added next.

---
+++ ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _AboutComponentPartial : ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.aboutTitle = portfolioContext.Abouts.Select(a => a.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(a => a.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetails = portfolioContext.Abouts.Select(a => a.Details).FirstOrDefault();
+
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using NuGet.Protocol;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioContext.Skills.ToList();
+            return View(values);
+        }
+    }
+}

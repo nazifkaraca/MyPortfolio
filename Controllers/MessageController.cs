7cfb0710c5af4e21d09e18b1854d84e2f8bbbcc9
nazifkaraca <nazif808@gmail.com>
2024-11-15 17:21:29 +0300

    MyPortfolio: Message section added, message edit section completed.

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class MessageController : Controller
+	{
+		MyPortfolioContext portfolioContext = new MyPortfolioContext();
+		public IActionResult Inbox()
+		{
+			var values = portfolioContext.Messages.ToList();
+			return View(values);
+		}
+
+		public IActionResult ChangeIsReadToTrue(int id)
+		{
+			var value = portfolioContext.Messages.Find(id);
+			value.IsRead = true;
+			portfolioContext.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult ChangeIsReadToFalse(int id)
+		{
+			var value = portfolioContext.Messages.Find(id);
+			value.IsRead = false;
+			portfolioContext.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+	}
+}

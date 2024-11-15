470203270347ed1f4504b611b953e28954fb640a
nazifkaraca <nazif808@gmail.com>
2024-11-15 15:53:46 +0300

    MyPortfolio: Layout changes implemented.

---
+++ ViewComponents/LayoutViewComponents/_LayoutSideBarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutSideBarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
